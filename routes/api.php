bb3799a6f661d11008a62ea32f5bbc52dd9c1c41
DanielC <danielcordero1998@gmail.com>
2023-10-18 20:48:59 -0400

    add users crud

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Auth Group
+Route::prefix( '/user' )->group( function () {
+    Route::get('/login', [UsersController::class,'login']);
+    Route::post('/register', [UsersController::class,'create']);
 });
